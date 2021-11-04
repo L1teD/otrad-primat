67b64931aeb6614ff8bc629a0770aff5db662a76
L1teD <89979325+L1teD@users.noreply.github.com>
2021-11-05 00:42:58 +0900

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+ 	<meta charset="utf-8">
+	<title>admin</title>
+ </head>
+ <body>
+ 	<?php echo '<p>Ты чё бля, хотел получить доступ к сайту?
+	ага конечно, хуй тебе а не админка малолетний еблан</p>'; ?>
+ </body>
+</html>
